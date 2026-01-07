f3be7da910efc82ce314a20c90d12158736fe78a
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-01-07 10:38:15 +0700

    n0kz_attr: fixup semantic logic for dirty pte guard
    Rebranded from e404_attr.
    
    Co-authored-by: bimoalfarrabi <bimoalfarrabi24@gmail.com>
    
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -166,8 +166,12 @@ static inline pte_t pte_mkwrite(pte_t pte)
 {
 	pte = set_pte_bit(pte, __pgprot(PTE_WRITE));
 #ifdef CONFIG_N0KZ_SIGNATURE
-	if ((pte_sw_dirty(pte)) && (n0kz_data.avoid_dirty_pte == 1))
+	if (n0kz_data.avoid_dirty_pte == 1) {
+		if (pte_sw_dirty(pte))
+			pte = clear_pte_bit(pte, __pgprot(PTE_RDONLY));
+	} else {
 		pte = clear_pte_bit(pte, __pgprot(PTE_RDONLY));
+	}
 #else
 	pte = clear_pte_bit(pte, __pgprot(PTE_RDONLY));
 #endif
